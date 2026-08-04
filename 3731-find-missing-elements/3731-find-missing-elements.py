76231a838d390468d3286448b67bba1ccb840e14
Vaishnavi Gupta <137624508+Vaishnavi698@users.noreply.github.com>
2026-08-04 13:34:49 +0530

    Time: 3 ms (69.59%), Space: 19.4 MB (18.85%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,19 @@
+#Ques
+#You are given an integer array nums consisting of unique integers.Originally, nums contained every integer within a certain range. However, some integers might have gone missing from the array.The smallest and largest integers of the original range are still present in nums.Return a sorted list of all the missing integers in this range. If no integers are missing, return an empty list.
+
+#Solution
+
+from typing import List
+
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        num_set = set(nums)
+        low, high = min(nums), max(nums)
+        
+      
+        missing = []
+        for x in range(low, high + 1):
+            if x not in num_set:
+                missing.append(x)
+                
+        return missing
